c19bebc642cc65cb4bc49bd3329b585f2bf18c19
renzhifan <zhifan6797@163.com>
2021-05-06 18:30:50 +0800

    up

--- chapter01/01-variable.go
+++ chapter01/01-variable.go
@@ -3,17 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	var v1 int            // 整型
-	var v2 string         // 字符串
-	var v3 bool           // 布尔型
-	var v4 [10]int        // 数组，数组元素类型为整型
-	var v5 struct {       // 结构体，成员变量 f 的类型为64位浮点型
+	var v1 int      // 整型
+	var v2 string   // 字符串
+	var v3 bool     // 布尔型
+	var v4 [10]int  // 数组，数组元素类型为整型
+	var v5 struct { // 结构体，成员变量 f 的类型为64位浮点型
 		f float64
 	}
-	var v6 *int           // 指针，指向整型
-	var v7 map[string]int   // map（字典），key为字符串类型，value为整型
-	var v8 func(a int) int  // 函数，参数类型为整型，返回值类型为整型
+	var v6 *int            // 指针，指向整型
+	var v7 map[string]int  // map（字典），key为字符串类型，value为整型
+	var v8 func(a int) int // 函数，参数类型为整型，返回值类型为整型
 
+	v9 := 2
+	v1 , v9 = v9 , v1
 	// 打印上述变量值
 	fmt.Printf("v1: %v\n", v1)
 	fmt.Printf("v2: %v\n", v2)
@@ -23,4 +25,5 @@ func main() {
 	fmt.Printf("v6: %v\n", v6)
 	fmt.Printf("v7: %v\n", v7)
 	fmt.Printf("v8: %v\n", v8)
+	fmt.Printf("v9: %v\n", v9)
 }
