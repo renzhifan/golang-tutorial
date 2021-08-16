8a2ab88c699db6b7726370ff4e8bcaa7d1c02e82
renzhifan <renzhifan@haohaozhu.com>
2021-08-16 10:51:02 +0800

    Default Changelist

--- chapter04/interface/interface_assign.go
+++ chapter04/interface/interface_assign.go
@@ -1,46 +1,72 @@
 package main
 
 import (
-    "fmt"
-    "reflect"
+	"fmt"
+	"reflect"
 )
 
 type Number1 interface {
-    Equal(i int) bool
-    LessThan(i int) bool
-    MoreThan(i int) bool
+	Equal(i int) bool
+	LessThan(i int) bool
+	MoreThan(i int) bool
 }
-
 type Number2 interface {
-    Equal(i int) bool
-    MoreThan(i int) bool
-    LessThan(i int) bool
-    Add(i int)
+	Equal(i int) bool
+	MoreThan(i int) bool
+	LessThan(i int) bool
+	Add(i int)
 }
-
 type Number int
 
 func (n Number) Equal(i int) bool {
-    return int(n) == i
+	return int(n) == i
 }
 
 func (n Number) LessThan(i int) bool {
-    return int(n) < i
+	return int(n) < i
 }
 
 func (n Number) MoreThan(i int) bool {
-    return int(n) > i
+	return int(n) > i
 }
 
 func (n *Number) Add(i int) {
-    *n = *n + Number(i)
+	*n = *n + Number(i)
+}
+
+type Num1 interface {
+	Equal(i int) bool
+	LessThan(i int) bool
+	MoreThan(i int) bool
+}
+type Num2 interface {
+	Equal(i int) bool
+	LessThan(i int) bool
+	MoreThan(i int) bool
+}
+
+type Num int
+
+func (n Num) Equal(i int) bool {
+	return int(n) == i
+}
+func (n Num) LessThan(i int) bool {
+	return int(n) < i
+}
+func (n Num) MoreThan(i int) bool {
+	return int(n) > i
 }
 
 func main() {
-    var num1 Number = 1
-    var num2 Number2 = &num1
-    if num3, ok := num2.(Number1); ok {
-        fmt.Println(num3.Equal(1))
-        fmt.Println(reflect.TypeOf(num3))
-    }
+	var number1 Number = 1
+	var number2 Number2 = &number1
+	if number3, ok := number2.(Number1); ok {
+		fmt.Println(number3.Equal(1))
+		fmt.Println(reflect.TypeOf(number3))
+	}
+	var num Num = 1
+	var num1 Num1 = num
+	if num2, ok := num1.(Num2); ok {
+		fmt.Println(num2.Equal(1))
+	}
 }
--- chapter04/interface/struct_assign.go
+++ chapter04/interface/struct_assign.go
@@ -4,22 +4,24 @@ import "fmt"
 
 type Integer int
 
+// 加法运算
 func (a *Integer) Add(b Integer) {
-    *a = (*a) + b
+	*a = (*a) + b
 }
 
+// 乘法运算
 func (a Integer) Multiply(b Integer) Integer {
-    return a * b
+	return a * b
 }
 
 type Math interface {
-    Add(i Integer)
-    Multiply(i Integer) Integer
+	Add(i Integer)
+	Multiply(i Integer) Integer
 }
 
 func main() {
-    var a Integer = 1
-    var m Math = &a
-    m.Add(2)
-    fmt.Printf("1 + 2 = %d\n", a)
+	var a Integer = 2
+	var m Math = &a
+	m.Add(2)
+	fmt.Printf("1 + 2 = %d\n", a)
 }
