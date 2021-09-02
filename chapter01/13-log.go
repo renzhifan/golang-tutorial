0d3118b2f234c61b62ecdcbf79f8b7c204c056bf
renzhifan <zhifan6797@163.com>
2021-09-02 14:38:21 +0800

    使用viper 和log

--- chapter01/13-log.go
+++ chapter01/13-log.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
+	"time"
 )
 
 func main() {
 
-	logFile, err := os.Create("./log.log")
+	fileName := "./storage/" + time.Now().Format("2006-01-02") + ".log"
+	fmt.Println(fileName)
+	logFile, err := os.Create(fileName)
 	defer logFile.Close()
 	if err != nil {
 		log.Fatalln("create file log.log failed")
@@ -16,8 +20,8 @@ func main() {
 	logger := log.New(logFile, "[Debug]", log.Lshortfile)
 	logger.Println("debug info is ,check list ,hello")
 	logger.Println("debug info is ,check list ,hello000111")
-	logger.SetPrefix("[Info]")
-	logger.SetFlags(log.Ldate)
+	//logger.SetPrefix("[Info]")
+	logger.SetFlags(log.Ltime)
 	logger.SetOutput(logFile)
 	logger.Print("Info check")
 	logger.SetOutput(os.Stdout)
